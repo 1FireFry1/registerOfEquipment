f3ac38efce728f6d0b5828c86e96f1f8a965f3b8
С Игорь <igafry@gmail.com>
2022-03-30 22:35:46 +0700

    init

---
+++ src/main/java/com/firefry/registerOfEquipment/RegisterOfEquipmentApplication.java
@@ -0,0 +1,13 @@
+package com.firefry.registerOfEquipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterOfEquipmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterOfEquipmentApplication.class, args);
+	}
+
+}
